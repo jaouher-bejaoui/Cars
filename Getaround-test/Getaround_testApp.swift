ab82b92c1ef3372b6298d2ad2ec821fcb4b4f91e
Jaouher <Jaouher@MacBook-Air-de-Elisa.local>
2024-06-20 19:54:39 +0200

    ios-getaround-jaouher-create-project

---
+++ Getaround-test/Getaround_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Getaround_testApp.swift
+//  Getaround-test
+//
+//  Created by Jaouher  on 20/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Getaround_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
